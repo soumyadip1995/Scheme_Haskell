d0289badf8d8d5dde7e869774fcfac15654c4531
soumyadip1995 <30930746+soumyadip1995@users.noreply.github.com>
2020-10-31 11:09:46 +0530

    whitespace support

--- main.hs
+++ main.hs
@@ -1,17 +1,22 @@
-
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+
+spaces :: Parser ()
+spaces = skipMany1 space
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
 
 main :: IO ()
 main = do 
-  (expr:_) <- getArgs
-  putStrLn (readExpr expr)
+         (expr:_) <- getArgs
+         putStrLn (readExpr expr)
 
