8a523e4bb1e0b438a8447ab1afe740cedde0dd8a
soumyadip1995 <30930746+soumyadip1995@users.noreply.github.com>
2020-10-31 13:52:18 +0530

    initial evaluator updated

--- eval.hs
+++ eval.hs
@@ -68,8 +68,6 @@ showVal (Atom name) = name
 showVal (Number contents) = show contents
 showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
-
-
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 
